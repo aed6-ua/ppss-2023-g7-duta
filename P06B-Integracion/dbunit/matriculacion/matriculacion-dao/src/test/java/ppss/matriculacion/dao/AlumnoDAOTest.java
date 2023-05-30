dcb50308d9c1b51734f423291a06ddefb4bc83b1
aed6-ua <aed6@alu.ua.es>
2023-05-30 20:42:10 +0200

    p06B

---
+++ P06B-Integracion/dbunit/matriculacion/matriculacion-dao/src/test/java/ppss/matriculacion/dao/AlumnoDAOTest.java
@@ -0,0 +1,23 @@
+package ppss.matriculacion.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class AlumnoDAOTest {
+    @Test
+    @Tag("Integracion-fase1")
+    public void test1() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    @Tag("Integracion-fase1")
+    public void test2() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    @Tag("Integracion-fase1")
+    public void test3() {
+        Assertions.assertTrue(true);
+    }
+}
